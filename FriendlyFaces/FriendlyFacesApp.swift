afa29304130237637eb625259fd947d522d115e7
AtinAgnihotri <atinagnihotri@gmail.com>
2021-08-19 23:17:26 +0530

    Initial Commit

---
+++ FriendlyFaces/FriendlyFacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendlyFacesApp.swift
+//  FriendlyFaces
+//
+//  Created by Atin Agnihotri on 19/08/21.
+//
+
+import SwiftUI
+
+@main
+struct FriendlyFacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
