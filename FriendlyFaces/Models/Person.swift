4f94ab27920f39abd633cacdb32f6c544f08d1fd
AtinAgnihotri <atinagnihotri@gmail.com>
2021-08-19 23:23:21 +0530

    Adds Person model

---
+++ FriendlyFaces/Models/Person.swift
@@ -0,0 +1,21 @@
+//
+//  Person.swift
+//  FriendlyFaces
+//
+//  Created by Atin Agnihotri on 19/08/21.
+//
+
+import Foundation
+
+struct Person: Codable {
+    var id: UUID
+    var isActive: Bool
+    var age: Int
+    var company: String
+    var email: String
+    var address: String
+    var about: String
+    var registered: Date
+    var tags: [String]
+    
+}
